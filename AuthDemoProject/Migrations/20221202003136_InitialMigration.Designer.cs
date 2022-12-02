444ec6cb0c38605559d57f3a8899c2ad1d5b4aa9
Jordon P <jordon.peyton17@gmail.com>
2022-12-01 18:36:08 -0600

    configured app for mysql server

--- AuthDemoProject/Migrations/20221202003136_InitialMigration.Designer.cs
+++ AuthDemoProject/Migrations/20221202003136_InitialMigration.Designer.cs
@@ -9,7 +9,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AuthDemoProject.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20220602194302_InitialMigration")]
+    [Migration("20221202003136_InitialMigration")]
     partial class InitialMigration
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
